6e4cb8b4fadf00d92cf5261b739323b4386144af
RamSailopal <thelinuxguys@yahoo.com>
2022-02-28 11:30:31 +0000

    No need for sed anymore

--- reactenter/entrypoint.sh
+++ reactenter/entrypoint.sh
@@ -2,6 +2,5 @@
 curl -sL https://deb.nodesource.com/setup_14.x | bash -
 apt-get install -y nodejs
 cd /home/maps2
-sed -i 's#^.*proxy.*$#\t"proxy":"'$apiadd'",#' package.json
 npm install
 npm start
