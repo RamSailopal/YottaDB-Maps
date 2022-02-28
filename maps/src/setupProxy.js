a1a7c61f696840dfcd039d2146b05f9fdac97eca
RamSailopal <thelinuxguys@yahoo.com>
2022-02-28 12:18:56 +0000

    Prefix environmental variables with REACT_APP

--- maps/src/setupProxy.js
+++ maps/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
 	  app.use(
 		      '/api',
 		      createProxyMiddleware({
-			            target: process.env.apiadd,
+			            target: process.env.REACT_APP_APIADD,
 			            changeOrigin: true,
 			          })
 		    );
